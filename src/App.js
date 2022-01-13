c423cfef561fb8e7e775e760670f59587d43e20a
Wolftail8192 <4420299@gmail.com>
2022-01-13 19:36:23 +0200

    less1_cw

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 function App() {
 
   useEffect (()=>{
+    fetch('https://api.spacexdata.com/v3/launches/')
+        .then(value => value.json())
+        .then(value => {
+          console.log(value)
+        })
 
-  })
+  },[])
 
   return (
     <div className="App">
